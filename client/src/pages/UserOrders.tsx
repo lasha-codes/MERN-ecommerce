689fff718ae409fb3ff3e099bbe32aa45d2d9eb5
lasha-codes <lashagiorgi420@gmail.com>
2024-03-21 23:58:24 -0700

    getting back orders data from the api

--- client/src/pages/UserOrders.tsx
+++ client/src/pages/UserOrders.tsx
@@ -1,5 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useContext } from 'react'
+import { userContext } from '../components/UserContext'
+import Header from '../components/Header'
+import Loader from '../components/Loader'
+
 const UserOrders = () => {
-  return <div>UserOrders</div>
+  const { orders } = useContext<any>(userContext)
+  return (
+    <main className='bg-gray-100 w-full h-screen pt-32'>
+      <Header />
+      <div>
+        {orders.length > 0 ? (
+          orders.map((productInfo: any, idx: number) => {
+            return (
+              <div key={idx}>
+                <h3>product</h3>
+                {productInfo.products.map((product: any, idx: number) => {
+                  return <span key={idx}>{product.productTitle}</span>
+                })}
+              </div>
+            )
+          })
+        ) : (
+          <Loader />
+        )}
+      </div>
+    </main>
+  )
 }
 
 export default UserOrders
