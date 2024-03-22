6005ff0ef940b08de6f3eab81e3573b0b9d9b12a
lasha-codes <lashagiorgi420@gmail.com>
2024-03-22 12:25:55 -0700

    everything working fine displaying orders length and orders

--- client/src/pages/UserSigned.tsx
+++ client/src/pages/UserSigned.tsx
@@ -8,70 +8,74 @@ import Header from '../components/Header.js'
 import UserInfoComponent from '../components/UserInfoComponent.js'
 import EditProfileComponent from '../components/EditProfileComponent.js'
 
-const profileData = [
-  {
-    name: 'Wishlist',
-    icon: (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-        strokeWidth='1.5'
-        stroke='currentColor'
-        className='w-8 h-8'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
-        />
-      </svg>
-    ),
-    to: '/user-wishlist',
-  },
-  {
-    name: 'Orders',
-    icon: (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-        strokeWidth={1.5}
-        stroke='currentColor'
-        className='w-8 h-8'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25  2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z'
-        />
-      </svg>
-    ),
-    to: '/user/orders',
-  },
-  {
-    name: '1,927.53',
-    icon: (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        viewBox='0 0 24 24'
-        strokeWidth={1.5}
-        stroke='currentColor'
-        className='w-8 h-8'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          d='M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
-        />
-      </svg>
-    ),
-  },
-]
-
 const UserSigned = () => {
-  const { activeRoute } = useContext<any>(userContext)
+  const { activeRoute, orders, user } = useContext<any>(userContext)
+
+  const userOrders = orders.filter((order: any) => {
+    return user.emailContext === order.email
+  })
+
+  const profileData = [
+    {
+      name: 'Wishlist',
+      icon: (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='currentColor'
+          className='w-8 h-8'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
+          />
+        </svg>
+      ),
+      to: '/user-wishlist',
+    },
+    {
+      name: 'Orders',
+      icon: (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-8 h-8'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25  2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z'
+          />
+        </svg>
+      ),
+      to: '/user/orders',
+    },
+    {
+      name: '1,927.53',
+      icon: (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-8 h-8'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+          />
+        </svg>
+      ),
+    },
+  ]
 
   return (
     <main className='w-full h-screen screen flex flex-col items-center overflow-scroll bg-gray-200'>
@@ -98,7 +102,7 @@ const UserSigned = () => {
                   {item.icon}
                   {item.name === 'Orders' ? (
                     <div className='absolute text-xs flex items-center justify-center h-[18px] w-[18px] right-2 rounded-full bg-[#e65848] text-white font-[400] top-0'>
-                      0
+                      {userOrders.length}
                     </div>
                   ) : null}
                   <span>{item.name}</span>
